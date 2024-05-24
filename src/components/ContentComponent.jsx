095bd20100c693795da09b765dd08ced6eeff28b
Mallarino <153091450+Mallarino@users.noreply.github.com>
2024-05-24 13:20:40 -0500

    Creacion de plantilla con React

---
+++ src/components/ContentComponent.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Content = () => (
+    <div className="col-sm-8">
+      <h2>TITLE HEADING</h2>
+      <h5>Title description, Dec 7, 2020</h5>
+      <div className="fakeimg" style={{ height: "200px", background: "#aaa" }}>Fake Image</div>
+      <p>Some text..</p>
+      <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+  
+      <h2 className="mt-5">TITLE HEADING</h2>
+      <h5>Title description, Sep 2, 2020</h5>
+      <div className="fakeimg" style={{ height: "200px", background: "#aaa" }}>Fake Image</div>
+      <p>Some text..</p>
+      <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+    </div>
+  );
+
+export default Content;
---
+++ src/components/FooterComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+const Footer = () => (
+    <div className="mt-5 p-4 bg-dark text-white text-center">
+      <p>Footer</p>
+    </div>
+  );
+
+  export default Footer;
